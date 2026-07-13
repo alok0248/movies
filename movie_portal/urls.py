1144a87b92184ff8fae116870bbc0b25200c6f58
alok0248 <alok.ak639@gmail.com>
2026-07-13 14:30:17 +0530

    Add media file serving for development

--- movie_portal/urls.py
+++ movie_portal/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.shortcuts import redirect
 from django.urls import path, include
 
@@ -10,3 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
